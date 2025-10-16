f58cadd0f70a4de8c3edade5e2075a8486d27229
roynx98 <roynx98@gmail.com>
2025-10-15 23:21:29 -0600

    chore: Dockerize app

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:23.6.1
+
+USER root
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    chromium \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY package*.json ./
+
+RUN npm install --production
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-import puppeteer from 'puppeteer';
-import express from 'express';
-import bodyParser from 'body-parser';
+import puppeteer from "puppeteer";
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+const environment = process.env.NODE_ENV;
+const isProduction = environment === "production";
+
 app.use(bodyParser.json());
 
-app.post('/', async (req, res) => {
+const browser = await puppeteer.launch({
+  headless: true,
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  ...(isProduction && { executablePath: process.env.CHROMIUM_PATH }),
+});
+
+app.post("/", async (req, res) => {
   const { username, password } = req.body;
 
-  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.ta-retirement.com/');
-  await page.setViewport({width: 1080, height: 1024});
+  await page.goto("https://www.ta-retirement.com/");
+  await page.setViewport({ width: 1080, height: 1024 });
   await page.locator('input[name="txtUsername"]').fill(username);
   await page.locator('input[name="txtPassword"]').fill(password);
-  await page.locator('.submitButton').click();
-  await page.locator('#securityCode').wait();
-  const header = await page.locator('h1').waitHandle();
-  const fullTitle = await header.evaluate(el => el.textContent);
+  await page.locator(".submitButton").click();
+  await page.locator("#securityCode").wait();
+  const header = await page.locator("h1").waitHandle();
+  const fullTitle = await header.evaluate((el) => el.textContent);
   await browser.close();
   res.send(fullTitle);
 });
 
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+  console.log("Servidor corriendo en http://localhost:3000");
 });
-
-
