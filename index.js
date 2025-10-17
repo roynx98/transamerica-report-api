7afeb60e9480de7357ccce89a33ecffd0fcb1978
roynx98 <roynx98@gmail.com>
2025-10-17 09:42:38 -0600

    feat: Download file.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 import express from "express";
 import bodyParser from "body-parser";
+import fs from "fs";
 
 const app = express();
 const environment = process.env.NODE_ENV;
@@ -13,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/get-report", async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, starDate, endDate } = req.body;
 
   const browser = await puppeteer.launch({
     headless: true,
@@ -26,16 +27,34 @@ app.post("/get-report", async (req, res) => {
   });
 
   const page = await browser.newPage();
+
+  const cookies = JSON.parse(fs.readFileSync("./cookies.json", "utf8"));
+  await browser.setCookie(...cookies);
+
   await page.goto("https://www.ta-retirement.com/");
   await page.setViewport({ width: 1080, height: 1024 });
   await page.locator('input[name="txtUsername"]').fill(username);
   await page.locator('input[name="txtPassword"]').fill(password);
   await page.locator(".submitButton").click();
-  await page.locator("#securityCode").wait();
-  const header = await page.locator("h1").waitHandle();
-  const fullTitle = await header.evaluate((el) => el.textContent);
+  await page.locator("#m10_5").wait();
+  await page.goto(
+    "https://www.ta-retirement.com/SIP/Employer/PlanReports/Contribution/ps_ContributionRateChange.aspx?UserType=S"
+  );
+
+  const startDateInput = await page.$('#ucPlanReports_txtDateStart')
+  startDateInput.evaluate((el) => el.value = starDate);
+
+  const endDateInput = await page.$('#ucPlanReports_txtDateEnd')
+  endDateInput.evaluate((el) => el.value = endDate);
+
+  await page.locator('label[for="ucPlanReports_rblParticipants2_0"]').click();
+
+  await page.locator("#ucPlanReports_btnSubmit").click()
+
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
   await browser.close();
-  res.send(fullTitle);
+  res.send("Succed");
 });
 
 app.listen(3001, () => {
